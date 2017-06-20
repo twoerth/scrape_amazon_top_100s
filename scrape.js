330605cb977686ebd19abe08ad0f78f07995e64d
Tristan Woerth <twoerth@gmail.com>
2017-06-20 23:28:37 +0200

    pushing to elasticsearch

--- scrape.js
+++ scrape.js
@@ -4,11 +4,15 @@ const cheerio = require('cheerio');
 
 // Movers and Shakers look like https://www.amazon.de/gp/movers-and-shakers/ce-de/ref=zg_bsms_pg_1?ie=UTF8&pg=1&ajax=1
 
-exports.find_product = ( html) => {
+exports.find_product = ( country, category, type, page, html ) => {
     $ = cheerio.load( html );
     return( $(".zg_itemImmersion").map( (i, p) => {
         const p_p = $(p);
         const product = {
+            country: country,
+            category: category,
+            type: type,
+            page: page,
             rank: p_p.find(".zg_rankNumber").text().trim(),
             change: p_p.find(".zg_salesMovement").text(),
             url: p_p.find(".p13n-asin a").first().attr('href'),
@@ -23,3 +27,31 @@ exports.find_product = ( html) => {
         return product;
     }).get() )
 }
+
+exports.generate_urls = ( country ) => {
+    const TYPES = [ "movers-and-shakers", "bestsellers", "new-releases" ]
+    const CATEGORIES = {
+        de: ["automotive", "baby", "diy", "beauty", "apparel", "lighting", "officeproduct", "computers", "drugstore", "appliances", "ce-de", "garden", "industrial", "pet-supplies", "photo", "luggage", "kitchen", "grocery", "musical-instruments", "jewelry", "shoes", "toys", "sports", "watch"]
+    }
+    const PAGES = [ 1, 2, 3, 4, 5]
+    
+    const URLS = [];
+    
+    for( const category of CATEGORIES[ country ] ) {
+        for( const type of TYPES ) {
+            for( const page of PAGES ) {
+                URLS.push( {
+                    url: "https://www.amazon." + country + "/gp/" + type + "/" + category + "/ref=zg_bsms_pg_1?ie=UTF8&pg=" + page + "&ajax=1",
+                    country: country,
+                    type: type,
+                    category: category,
+                    page: page
+                } );
+            }
+        }
+    }
+    
+    return URLS;
+}
+
+
--- scrape_test.js
+++ scrape_test.js
@@ -1,8 +1,16 @@
 "use strict";
 
-const fs=require('fs');
+const fs = require('fs');
 const scrape = require('./scrape.js');
 const request = require('request');
+
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+	host: 'http://elastic:changeme@127.0.0.1:9200',
+	requestTimeout: Infinity, // Tested
+	keepAlive: true // Tested
+});
 
 /*
 fs.readFile('test_data/ms_elec_0.html', 'utf8', (err, data) => {
@@ -11,9 +19,31 @@ fs.readFile('test_data/ms_elec_0.html', 'utf8', (err, data) => {
 });
 */
 
-//var url = "https://www.amazon.de/gp/movers-and-shakers/ce-de/ref=zg_bsms_pg_1?ie=UTF8&pg=1&ajax=1";
-var url = "https://www.amazon.com/gp/new-releases/electronics/ref=zg_bsms_pg_1?ie=UTF8&pg=1&ajax=1";
+//var url = "https://www.amazon.com/gp/movers-and-shakers/electronics/ref=zg_bsms_pg_1?ie=UTF8&pg=1&ajax=1";
+//var url = "https://www.amazon.com/gp/new-releases/electronics/ref=zg_bsms_pg_1?ie=UTF8&pg=1&ajax=1";
 
+/*
 request( url, ( error, response, html ) => {
   console.log( scrape.find_product( html ) )
 })
+*/
+
+for (const page of scrape.generate_urls("de")) {
+
+	request(page.url, (error, response, html) => {
+		const ops = []
+		for( const doc of scrape.find_product(page.country, page.category, page.type, page.page, html) ) {
+			ops.push({ index: { _index: 'myindex', _type: 'mytype' } })
+			ops.push( doc )
+		}
+		client.bulk(
+			{
+				body: ops
+			},
+			function(error, response) {
+				console.log( error );
+			}
+		);
+		
+	});
+}
